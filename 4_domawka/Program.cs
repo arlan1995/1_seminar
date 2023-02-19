f834619275cccd0206cf93e9bd37f80ac2a1e781
Arlan <earl199529@gmail.com>
2023-02-19 19:31:16 +0600

    Закончил с 1 домашкой

--- 4_domawka/Program.cs
+++ 4_domawka/Program.cs
@@ -1,27 +1,25 @@
 ﻿int n;
-              Console.WriteLine("N = ");
+        
+        Console.WriteLine("N = ");
         n = (int) inputValue();
         int index;
         
         index = 1;
-        while (index <= n )
-        
-            Console.Write(index.ToString() + " ");
-            index = (index + 1);
+        while (index < n)
+        {
+            if (index % 2 == 0)
             {
-                if (n % 2 == 0 && n != 0){
-                    Console.WriteLine("n");}    
-            
-                else
-                {
-                   
-                }
-                    
-      }
- double inputValue()
+                Console.WriteLine(index);
+                index = index + 1;
+            }
+            else
+            {
+                index = index + 1;
+            }
+        }
+double inputValue()
     {
         double result;
         while (!double.TryParse(Console.ReadLine(), out result));
         return result;
     }
-
