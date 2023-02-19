57baa027904ca70e72c4c8cdf93efc84a50775bf
Arlan <earl199529@gmail.com>
2023-02-19 19:30:56 +0600

    Решил 4 задачу)

--- 4_domawka/Program.cs
+++ 4_domawka/Program.cs
@@ -1,33 +1,27 @@
-﻿using System;
-
-public class MyProgram
-{
-    public static void Main(string[] args)
-    {
-        int n;
-        
-        Console.WriteLine("N = ");
+﻿int n;
+              Console.WriteLine("N = ");
         n = (int) inputValue();
         int index;
         
         index = 1;
         while (index <= n )
-        {
+        
             Console.Write(index.ToString() + " ");
             index = (index + 1);
-                if (index % 2 == 0)
-                    Console.Write("index");
+            {
+                if (n % 2 == 0 && n != 0){
+                    Console.WriteLine("n");}    
+            
                 else
-                    Console.Write("no");
+                {
+                   
+                }
+                    
       }
-    }
-    
-    // .NET can only read single characters or entire lines from the
-    // console. The following function safely reads a double value.
-    private static double inputValue()
+ double inputValue()
     {
         double result;
         while (!double.TryParse(Console.ReadLine(), out result));
         return result;
     }
-}
+
